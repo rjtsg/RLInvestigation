7e15f12d0a9eda439f6ba87b1dcb282769d0a9e6
rjtsg <rjtsg@users.noreply.github.com>
2019-11-07 23:53:14 +0100

    mastered sinus

--- power2Test.py
+++ power2Test.py
@@ -11,35 +11,51 @@ class nchain:
         self.stop = 100
         self.counter = 0
         self.done = False
+        self.reward1 = 0
+    
+
     def DO(self,action):
-        #remember = np.sin(self.state*2*np.pi/20)
+        def EvalFunc(x):
+            # return (x-3)**4-6*(x-3)**2-12*(x-3)+35
+            return (np.sin(x/20*2*np.pi)+1)
+        remember = EvalFunc(self.state)
         if action == 0: #buy
-            self.reward = np.sin((self.state+1)*2*np.pi/20) - np.sin(self.state*2*np.pi/20)
+            self.reward1 = EvalFunc((self.state+1)) - EvalFunc(self.state)
         elif action == 1: #sell
-            self.reward = np.sin(self.state*2*np.pi/20) - ((self.state+1)*2*np.pi/20)
+            self.reward1 = EvalFunc(self.state) - EvalFunc((self.state+1))
+        elif action == 2:
+            pass
         else:
             print('error')
+        # print(self.reward1)
+        # self.reward1 = self.reward1*2
         self.state += 1
         if self.state == 21:
             self.done = True
+            
+            # if self.reward1 >= :
+            #     self.reward = 10
+            # else:
+            #     self.reward = 0
+
         
-        
-        #return self.state, self.reward, self.done, remember
-        return self.state, self.reward, self.done
+        # return self.state, self.reward, self.done, remember
+        return self.state, self.reward1, self.done
     def reset(self):
         self.state = 0
         self.reward = 0
         self.stop = 100
         self.counter = 0
         self.done = False
+        self.reward1 = 0
         return self.state
 
 def q_learning_keras(env, num_episodes=1000):
     # create the keras model
     model = Sequential()
     model.add(InputLayer(batch_input_shape=(1, 22)))
-    model.add(Dense(10, activation='sigmoid'))
-    model.add(Dense(2, activation='linear'))
+    model.add(Dense(30, activation='sigmoid'))
+    model.add(Dense(3, activation='linear'))
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
     # now execute the q learning
     y = 0.95
@@ -55,7 +71,7 @@ def q_learning_keras(env, num_episodes=1000):
         r_sum = 0
         while not done:
             if np.random.random() < eps:
-                a = np.random.randint(0, 2)
+                a = np.random.randint(0, 3)
             else:
                 a = np.argmax(model.predict(np.identity(22)[s:s + 1]))
             new_s, r, done = env.DO(a)
@@ -63,7 +79,7 @@ def q_learning_keras(env, num_episodes=1000):
             target = r + y * np.amax(model.predict(np.identity(22)[new_s:new_s + 1]))
             target_vec = model.predict(np.identity(22)[s:s + 1])[0]
             target_vec[a] = target
-            model.fit(np.identity(22)[s:s + 1], target_vec.reshape(-1, 2), epochs=1, verbose=0)
+            model.fit(np.identity(22)[s:s + 1], target_vec.reshape(-1, 3), epochs=1, verbose=0)
             s = new_s
             r_sum += r
         r_avg_list.append(r_sum)
