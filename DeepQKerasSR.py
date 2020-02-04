f381b6823e32a2899aed22f2e11dc9f3bbb010ab
rjtsg <rjtsg@users.noreply.github.com>
2020-02-04 11:35:11 +0100

    minor update

--- DeepQKerasSR.py
+++ DeepQKerasSR.py
@@ -12,3 +12,18 @@ class DeepQKerasSR:
         self.y = 0.95
         self.eps = 0.5
         self.decay_factor = 0.999
+
+    def Action(self,observation):
+        if np.random.random() < self.eps:
+            action = np.random.randint(0, 3)
+        else:
+            action = np.argmax(self.model.predict(np.array([observation])))
+        return action
+
+    def Train(self, action,observation, new_observation, reward, done):
+        target = reward + self.y * np.amax(self.model.predict(np.array([new_observation])))*(1-int(done))
+        target_vec = self.model.predict(np.array([observation]))[0]
+        target_vec[action] = target
+        self.model.fit(np.array([observation]), target_vec.reshape(-1, 3), epochs=1, verbose=0)
+        if done == True:
+            self.eps *= self.decay_factor
