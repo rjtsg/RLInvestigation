5346c2011f58e8d8ddd4679997c2d16bc27b1770
rjtsg <rjtsg@users.noreply.github.com>
2020-02-06 16:57:25 +0100

    Added score keeping of stopping date

--- BenchMarkTest.py
+++ BenchMarkTest.py
@@ -18,6 +18,7 @@ tot_reward_list = []
 no_stock_list = []
 no_money_list = []
 NetWorth_list = []
+stop_day_list = []
 
 for i in range(num_episodes):
     observation, reward, done = env.reset()
@@ -36,17 +37,19 @@ for i in range(num_episodes):
     NetWorth_list.append(info[2])
     no_stock_list.append(info[1])
     no_money_list.append(info[0])
+    stop_day_list.append(observation[0])
     print('episode: {0:.0f}, reward score: {1:.2f}'.format(i, reward_score))
 
 dataframe_csv = {'Reward': tot_reward_list,
                  'No cash trade': no_money_list,
                  'No stock trade': no_stock_list,
-                 'Net worth end game': NetWorth_list}
+                 'Net worth end game': NetWorth_list,
+                 'Stop day': stop_day_list}
 
 
 df = pd.DataFrame(dataframe_csv)
 os.chdir('Trading_data')
-df.to_csv('Trading-DQKSR-24-3000-quiter-second_layer-applied_lr_0.001-V1.csv', index= False)
+df.to_csv('Trading-DQKSR-24-2000-quiter-second_layer-applied_lr_0.0001-V1.csv', index= False)
 os.chdir('..')
 
 plt.plot(tot_reward_list)
