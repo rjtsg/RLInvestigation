93d1cb2595b7063c433254c34c0ed9d28f56fb98
rjtsg <rjtsg@users.noreply.github.com>
2020-02-06 12:05:34 +0100

    Added longer runs

--- CartPole_data/results.py
+++ CartPole_data/results.py
@@ -1,11 +1,11 @@
 import matplotlib.pylab as plt
 import pandas as pd
 
-data1 = pd.read_csv('Cartpole-DQKSR-10-2000.csv')
-data2 = pd.read_csv('Cartpole-DQKSR-100-2000.csv')
-data3 = pd.read_csv('Cartpole-DQKSR-1000-2000.csv')
+data1 = pd.read_csv('Cartpole-DQKSR-5-2000.csv')
+data2 = pd.read_csv('Cartpole-DQKSR-10-2000.csv')
+data3 = pd.read_csv('Cartpole-DQKSR-15-2000.csv')
 
-plt.plot(data1['Net worth end game'])
-plt.plot(data2['Net worth end game'])
-plt.plot(data3['Net worth end game'])
+plt.plot(data1['Reward'])
+plt.plot(data2['Reward'])
+plt.plot(data3['Reward'])
 plt.show()
