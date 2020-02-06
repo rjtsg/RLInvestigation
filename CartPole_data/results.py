ce6832854ad965ee98ea759ef81a36b346a13f77
rjtsg <rjtsg@users.noreply.github.com>
2020-02-06 11:23:00 +0100

    added plot file

--- CartPole_data/results.py
+++ CartPole_data/results.py
@@ -1,9 +1,9 @@
 import matplotlib.pylab as plt
 import pandas as pd
 
-data1 = pd.read_csv('DQKSR-10-2000.csv')
-data2 = pd.read_csv('DQKSR-100-2000.csv')
-data3 = pd.read_csv('DQKSR-1000-2000.csv')
+data1 = pd.read_csv('Cartpole-DQKSR-10-2000.csv')
+data2 = pd.read_csv('Cartpole-DQKSR-100-2000.csv')
+data3 = pd.read_csv('Cartpole-DQKSR-1000-2000.csv')
 
 plt.plot(data1['Net worth end game'])
 plt.plot(data2['Net worth end game'])
