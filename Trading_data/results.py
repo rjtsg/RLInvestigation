9d5bb8043b1c8b31b3345074e75e46fc32d63953
rjtsg <rjtsg@users.noreply.github.com>
2020-02-10 14:10:02 +0100

    Processed DQKSR data

--- Trading_data/results.py
+++ Trading_data/results.py
@@ -6,18 +6,18 @@ import os
 data = []
 data_smooth = []
 N = 50
-save_file = True
-save_name = 'CartPole-DQKSR-10_nodes-5000-multiple-filtered.png'
-plt.title('Cartpole DQKSR comparison filtered')
+save_file = False
+save_name = 'Trading-DQKSR-xx_nodes-2000-lr_0.0001-quiter-filtered.png'
+plt.title('Trading DQKSR comparison filtered\n Environment: Quiter\n LR: 0.0001')
 plt.xlabel('Number of episodes')
 plt.ylabel('Reward')
-LEGEND = ['Run 1', 'Run 2', 'Run 3', 'Run 4', 'Run 5']
+LEGEND = ['24 nodes', '48 nodes', '100 nodes']
 
-data.append(pd.read_csv('Cartpole-DQKSR-10-5000-1.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-10-5000-2.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-10-5000-3.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-10-5000-4.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-10-5000-5.csv'))
+data.append(pd.read_csv('Trading-DQKSR-24-2000-quiter-second_layer-applied_lr_0.0001-V1.csv'))
+data.append(pd.read_csv('Trading-DQKSR-48-2000-quiter-second_layer-applied_lr_0.0001-V1.csv'))
+data.append(pd.read_csv('Trading-DQKSR-100-2000-quiter-second_layer-applied_lr_0.0001-V1.csv'))
+
+
 
 for i in range(len(data)):
     data_smooth.append(np.convolve(data[i]['Reward'], np.ones((N,))/N, mode='valid'))
@@ -27,7 +27,7 @@ plt.legend(LEGEND)
 
 
 if save_file == True:
-    os.chdir('CartPole Figures')
+    os.chdir('Trading Figures')
     duplicate = os.path.exists(save_name)
     if duplicate == True:
         print('The file name is already in use, it will not be saved')
