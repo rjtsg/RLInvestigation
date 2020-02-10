29c0f353d09352986d7d3a217222dc813860788b
rjtsg <rjtsg@users.noreply.github.com>
2020-02-10 11:35:29 +0100

    Added code for smoothening

--- CartPole_data/results.py
+++ CartPole_data/results.py
@@ -1,29 +1,35 @@
 import matplotlib.pylab as plt
 import pandas as pd
+import numpy as np
 import os
 
-data1 = pd.read_csv('Cartpole-DQKSR-5-2000.csv')
-data2 = pd.read_csv('Cartpole-DQKSR-10-2000.csv')
-data3 = pd.read_csv('Cartpole-DQKSR-15-2000.csv')
-data4 = pd.read_csv('Cartpole-DQKSR-25-2000.csv')
-data5 = pd.read_csv('Cartpole-DQKSR-50-2000.csv')
-data6 = pd.read_csv('Cartpole-DQKSR-100-2000.csv')
+data = []
+data_smooth = []
+N = 25
+
+data.append(pd.read_csv('Cartpole-DQKSR-5-2000.csv'))
+data.append(pd.read_csv('Cartpole-DQKSR-10-2000.csv'))
+data.append(pd.read_csv('Cartpole-DQKSR-15-2000.csv'))
+data.append(pd.read_csv('Cartpole-DQKSR-25-2000.csv'))
+data.append(pd.read_csv('Cartpole-DQKSR-50-2000.csv'))
+data.append(pd.read_csv('Cartpole-DQKSR-100-2000.csv'))
+
+for i in range(len(data)):
+    data_smooth.append(np.convolve(data[i]['Reward'], np.ones((N,))/N, mode='valid'))
+    # plt.plot(data[i]['Reward'])
+    plt.plot(data_smooth[i])
 
 
-plt.plot(data1['Reward'])
-plt.plot(data2['Reward'])
-plt.plot(data3['Reward'])
-plt.plot(data4['Reward'])
-plt.plot(data5['Reward'])
-plt.plot(data6['Reward'])
 
+
+# plt.legend(['no filter', 'filter'])
 plt.legend(['5 nodes','10 nodes', '15 nodes', '25 nodes',
             '50 nodes', '100 nodes'])
 
-plt.title('Cartpole DQKSR comparison')
+plt.title('Cartpole DQKSR comparison filtered')
 plt.xlabel('Number of episodes')
 plt.ylabel('Reward')
-os.chdir('CartPole Figures')
-plt.savefig('CartPole-DQKSR-2000-2.png')
-os.chdir('..')
+# os.chdir('CartPole Figures')
+# plt.savefig('CartPole-DQKSR-2000-filtered.png')
+# os.chdir('..')
 plt.show()
