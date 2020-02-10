f88b504fb57bb9f829b0e7b053e431aae1c8ad4f
rjtsg <rjtsg@users.noreply.github.com>
2020-02-10 11:51:50 +0100

    prrt

--- CartPole_data/results.py
+++ CartPole_data/results.py
@@ -5,31 +5,33 @@ import os
 
 data = []
 data_smooth = []
-N = 25
-
-data.append(pd.read_csv('Cartpole-DQKSR-5-2000.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-10-2000.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-15-2000.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-25-2000.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-50-2000.csv'))
-data.append(pd.read_csv('Cartpole-DQKSR-100-2000.csv'))
-
-for i in range(len(data)):
-    data_smooth.append(np.convolve(data[i]['Reward'], np.ones((N,))/N, mode='valid'))
-    # plt.plot(data[i]['Reward'])
-    plt.plot(data_smooth[i])
-
-
-
-
-# plt.legend(['no filter', 'filter'])
-plt.legend(['5 nodes','10 nodes', '15 nodes', '25 nodes',
-            '50 nodes', '100 nodes'])
-
+N = 100
+save_file = False
+save_name = 'CartPole-DQKSR-5000-filtered.png'
 plt.title('Cartpole DQKSR comparison filtered')
 plt.xlabel('Number of episodes')
 plt.ylabel('Reward')
-# os.chdir('CartPole Figures')
-# plt.savefig('CartPole-DQKSR-2000-filtered.png')
-# os.chdir('..')
+LEGEND = ['5 nodes','10 nodes', '15 nodes']
+
+data.append(pd.read_csv('Cartpole-DQKSR-5-5000.csv'))
+data.append(pd.read_csv('Cartpole-DQKSR-10-5000.csv'))
+data.append(pd.read_csv('Cartpole-DQKSR-15-5000.csv'))
+
+
+for i in range(len(data)):
+    data_smooth.append(np.convolve(data[i]['Reward'], np.ones((N,))/N, mode='valid'))
+    plt.plot(data_smooth[i])
+
+plt.legend(LEGEND)
+
+
+if save_file == True:
+    os.chdir('CartPole Figures')
+    duplicate = os.path.exists(save_name)
+    if duplicate == True:
+        print('The file name is already in use, it will not be saved')
+    else:
+        print('Saving figure as:', save_name)
+        plt.savefig(save_name)
+    os.chdir('..')
 plt.show()
