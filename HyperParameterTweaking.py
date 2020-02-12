ce630a2ac6f9c0e13243fdbb10b6e7aa04a20df9
rjtsg <rjtsg@users.noreply.github.com>
2020-02-12 08:53:13 +0100

    Made creation of environments possible

--- HyperParameterTweaking.py
+++ HyperParameterTweaking.py
@@ -21,10 +21,18 @@ class SetupStudyParameters:
                 #Type, Year, Stock ticker, (if needed type of env (quiter))
                 EnvRex = re.compile(r'\w*')
                 GROUPS = EnvRex.findall(self.Environment_type)
-                print(GROUPS)
-                if GROUPS.group[0] == 'Trading':
+                if GROUPS[0] == 'Trading':
                     #Build trading env
+                    self.DataCreation = CreateTestTrainData() #Right now it only creates AXP stocks
+                    TrainYearRex = re.compile(r'(\d\d\d\d)(\w*)') #Regex to get training year and ticker data
+                    self.TrainingYear = TrainYearRex.search(self.Environment_type).group(1)
+                    self.StockTicker = TrainYearRex.search(self.Environment_type).group(2)
+                    self.train_data, x = self.DataCreation.TrainData(self.TrainingYear)
+                    self.env_mode = GROUPS[-2] #gets the type of Trading environment that needs to be created.
+                    self.env = TradingEnvironment(self.train_data, self.env_mode)
                     pass
-                elif GROUPS.group[0] == 'CartPole':
-                    env = gym.make('CartPole-v0')
+                elif GROUPS[0] == 'CartPole':
+                    self.env = gym.make('CartPole-v0')
 
+Run = SetupStudyParameters('Trading-2014AXP-normal', 'DQKSR', [10,10],[0.0001],
+                            [0.99],1000)
