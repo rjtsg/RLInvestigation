777880200fcf52bdc2f5540b0a853fe9f4ce102c
rjtsg <rjtsg@users.noreply.github.com>
2020-02-12 09:33:30 +0100

    Can start training

--- HyperParameterTweaking.py
+++ HyperParameterTweaking.py
@@ -1,5 +1,6 @@
 import matplotlib.pylab as plt
 import pandas as pd
+import numpy as np
 import os
 import re
 import gym
@@ -11,28 +12,63 @@ from AC_keras_phil import ACKeras
 class SetupStudyParameters:
     def __init__(self, Environment_type, Agent_type, layer_size, 
                  learning_rate, discount, num_episodes):
-                self.Environment_type = Environment_type
-                self.Agent_type = Agent_type
-                self.layer_size = layer_size
-                self.learning_rate = learning_rate
-                self.discount = discount
-                self.num_episodes = num_episodes
-                #Use regex to decompose Environment type into: 
-                #Type, Year, Stock ticker, (if needed type of env (quiter))
-                EnvRex = re.compile(r'\w*')
-                GROUPS = EnvRex.findall(self.Environment_type)
-                if GROUPS[0] == 'Trading':
-                    #Build trading env
-                    self.DataCreation = CreateTestTrainData() #Right now it only creates AXP stocks
-                    TrainYearRex = re.compile(r'(\d\d\d\d)(\w*)') #Regex to get training year and ticker data
-                    self.TrainingYear = TrainYearRex.search(self.Environment_type).group(1)
-                    self.StockTicker = TrainYearRex.search(self.Environment_type).group(2)
-                    self.train_data, x = self.DataCreation.TrainData(self.TrainingYear)
-                    self.env_mode = GROUPS[-2] #gets the type of Trading environment that needs to be created.
-                    self.env = TradingEnvironment(self.train_data, self.env_mode)
-                    pass
-                elif GROUPS[0] == 'CartPole':
-                    self.env = gym.make('CartPole-v0')
+        self.Environment_type = Environment_type
+        self.Agent_type = Agent_type
+        self.layer_size = layer_size
+        self.learning_rate = learning_rate
+        self.discount = discount
+        self.num_episodes = num_episodes
+        #Use regex to decompose Environment type into: 
+        #Type, Year, Stock ticker, (if needed type of env (quiter))
+        ####Building enviroments####
+        EnvRex = re.compile(r'\w*')
+        GROUPS = EnvRex.findall(self.Environment_type)
+        if GROUPS[0] == 'Trading':
+            #Build trading env
+            self.DataCreation = CreateTestTrainData() #Right now it only creates AXP stocks
+            TrainYearRex = re.compile(r'(\d\d\d\d)(\w*)') #Regex to get training year and ticker data
+            self.TrainingYear = TrainYearRex.search(self.Environment_type).group(1)
+            self.StockTicker = TrainYearRex.search(self.Environment_type).group(2)
+            self.train_data, x = self.DataCreation.TrainData(self.TrainingYear)
+            self.env_mode = GROUPS[-2] #gets the type of Trading environment that needs to be created.
+            self.env = TradingEnvironment(self.train_data, self.env_mode)
+            self.n_actions = 3 #This is not ideal
+            self.input_dims = 4 #this is not ideal
+            pass
+        elif GROUPS[0] == 'CartPole':
+            self.env = gym.make('CartPole-v0')
+            self.n_actions = 2
+            self.input_dims = 4
+        else:
+            print('Environment_type not understoond, check name')
+            raise NameError 
+        #Create and error here... something like above?
+        
+        ####CREATE AGENT####
+        if Agent_type == 'DQKSR':
+            self.Agent = DeepQKerasSR(self.learning_rate[0], self.discount[0], self.n_actions, 
+                self.layer_size[0], self.layer_size[1], self.input_dims)
+        elif Agent_type == 'ACKeras':
+            self.Agent = ACKeras(self.learning_rate[0],self.learning_rate[1],self.discount[0],
+                self.n_actions, self.layer_size[0],self.layer_size[1], self.input_dims)
 
-Run = SetupStudyParameters('Trading-2014AXP-normal', 'DQKSR', [10,10],[0.0001],
+    def StartTraining(self):
+        self.tot_reward_list = []
+        for i in range(self.num_episodes):
+            observation, reward, done = self.env.reset()
+            reward_score = 0
+            while not done:
+                action = self.Agent.Action(observation)
+                new_observation, reward, done, info = self.env.step(action)
+                # print(reward.type())
+                self.Agent.Train(action, observation, new_observation, reward, done)
+                reward_score += reward
+                observation = new_observation
+            self.tot_reward_list.append(reward_score)
+            print('episode: {0:.0f}, average reward score: {1:.2f}'.format(i, np.mean(self.tot_reward_list[-100:])))
+
+                    
+
+Run = SetupStudyParameters('Trading-2014AXP-quiter', 'DQKSR', [10,10],[0.0001],
                             [0.99],1000)
+Run.StartTraining()
